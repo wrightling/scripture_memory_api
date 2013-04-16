35e14e80d9509aa6258800d73fa69f0d2e42946d
wrwright <warren@thewrightangle.com>
2013-04-16 15:02:30 -0600

    basic validation checks

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,14 @@
 class Card < ActiveRecord::Base
   attr_accessible :reference, :scripture, :subject
+
+  validates :scripture, presence: true
+  validate :at_least_one_subject_or_reference
+
+  private
+
+  def at_least_one_subject_or_reference
+    if String(subject).empty? && String(reference).empty?
+      errors[:base] << "Please include a reference or subject"
+    end
+  end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe Card do
+  it "is invalid if scripture is missing" do
+    card = Card.new(reference: "John 3:16",
+                    subject: "God's Love")
+    expect(card).to have(1).errors_on(:scripture)
+  end
+
+  it "is invalid if both reference and subject are missing" do
+    expect(Card.new(scripture: 'all have sinned')).to be_invalid
+  end
+
   context "card creation" do
     subject(:card) { Card.new(reference: "John 3:16",
                               scripture: "For God so loved..",
@@ -9,5 +19,7 @@ describe Card do
     it "changes the number of cards" do
       expect { card.save }.to change { Card.count }.by(1)
     end
+
+    it { should be_valid }
   end
 end
