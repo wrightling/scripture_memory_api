0c9347d01893f7ff8c7b87620e6bda9001cca5ee
wrwright <warren@thewrightangle.com>
2013-05-17 09:29:24 -0600

    removed code from categorizations controller spec that is now in the request spec

--- spec/controllers/categorizations_controller_spec.rb
+++ spec/controllers/categorizations_controller_spec.rb
@@ -1,20 +1,4 @@
 require 'spec_helper'
 
 describe Api::V1::CategorizationsController do
-  describe "#create" do
-    context "with valid categorizations" do
-      it "responds with a 201 status"
-      it "creates two categorizations if two are specified"
-    end
-
-    context "with only invalid categorizations" do
-      it "responds with a 422 status"
-      it "responds with error messages"
-    end
-
-    context "with partial success" do
-      it "responds with a 201 status"
-      it "provides error mesages for failures"
-    end
-  end
 end
